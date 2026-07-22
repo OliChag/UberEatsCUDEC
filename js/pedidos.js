b4fc18ce13cf6a1f45be398590b186507c90322d
OliChag <chagten.oliver@gmail.com>
2026-07-21 21:03:23 -0600

    Configuramos iconos por medio de json

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,4 +1,8 @@
 let contenidoPlatillo = "";
+document.addEventListener('DOMContentLoaded', () => {
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, { edge: 'right' });
+});
 db.collection("platillos").onSnapshot((datos) => {
      datos.docChanges().forEach((registro) => {
         if (registro.type === "added") {
